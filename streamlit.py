1f27974344b1daf802c12b6964841709d333974a
Streamlit-test-refresh <WenFoong.Teng@dell.com>
2025-07-03 10:39:22 +0800

    Create streamlit.py

---
+++ streamlit.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.image("Dell.jpg")
+st.title("Welcome to Dell Global Business Center ISG NPI")
+
+st.date_input("Transaction Date")
+st.radio("Your Department:", ["QA", "NPI", "Process"])
