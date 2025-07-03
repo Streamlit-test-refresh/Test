3bb2c9ece17839ce3d5e7b3fc8ba285374a2d226
Streamlit-test-refresh <WenFoong.Teng@dell.com>
2025-07-03 16:28:32 +0800

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,37 @@
 import streamlit as st
+import cv2
+import joblib
+from PIL import Image
+import numpy as np
 
-st.title("Welcome to Dell Global Business Center ISG NPI")
+st.image("Dell.jpg")
+st.title("Welcome to Dell ISG NPI")
+st.text("Crack Detector using KNN")
 
-st.date_input("Transaction Date")
-st.radio("Your Department:", ["QA", "NPI", "Process"])
+model = joblib.load("trained_model_KNN.pkl")
+
+def preprocess_image(img):
+    blurred = cv2.GaussianBlur(img, (5, 5), 0)
+    edges = cv2.Canny(blurred, 50, 150)
+    return edges
+ 
+def extract_features(img):
+    resized = cv2.resize(img, (64, 64))
+    return resized.flatten().reshape(1, -1)
+
+uploaded_file = st.file_uploader("Upload on image", type=["png", "jpg", "jpeg"])
+
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file).convert("L")
+    img_array = np.array(image)
+
+    st.image(image, caption="You have uploaded this image")
+
+    processed_image = preprocess_image(img_array)
+    features = extract_features(processed_image)
+
+    prediction = model.predict(features)[0]
+    label = "Positive" if prediction == 1 else "Negative"
+    st.success(f"**Prediction:** {label}")
+    st.balloons()
